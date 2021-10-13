2447e5e9a88aa7fa457b83408b95a3437d3f017c
Thiago Henrique <thenrique@gmail.com>
2021-10-13 18:46:42 +0000

    Fix s3 bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/CrazyTux/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket = "my-s3-bucket"
+  bucket = "${bucket_prefix}-my-s3-bucket"
   acl    = "private"
   bucket_prefix = var.prefix
   versioning = {
